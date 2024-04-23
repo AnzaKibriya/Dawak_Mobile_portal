62c8cf27ea06e0dd70e5994e6c924083df98d58e
Laxman kumar  Alakunta <la21301@corp.ojas-it.com>
2024-04-23 15:53:16 +0530

    minorchanges

--- src/main/java/Pages/DawakAppPatientModule.java
+++ src/main/java/Pages/DawakAppPatientModule.java
@@ -95,7 +95,8 @@ public class DawakAppPatientModule {
         mobileWait.until(ExpectedConditions.elementToBeClickable(confirmDeleteBtn)).click();
         Pages.MobileCommon().waitForElementsInteractions();
         mobileWait.until(ExpectedConditions.visibilityOfElementLocated(By.id(patientRemoveMessage)));
-        Assert.assertEquals(androidDriver.findElement(By.id(patientRemoveMessage)).getText(), "Patient removed successfully" );
-        test.log(Status.PASS, "successfully patient removed");
+        Assert.assertEquals(androidDriver.findElement(By.xpath(patientRemoveMessage)).getText(), "Patient removed successfully" );
+        test.log(Status.PASS, "patient removed successfully");
+
     }
 }
